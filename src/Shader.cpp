26dc692b0bdddb43c81e69415cf012aafc360580
Victor ONIC <victor.onic@outlook.fr>
2025-03-05 12:27:14 +0100

    Reformat Shader class

--- src/Shader.cpp
+++ src/Shader.cpp
@@ -1,149 +1,142 @@
 #include "Shader.h"
 
-Shader::Shader(const std::string& VS_path, const std::string& FS_path)
+Shader::Shader(const std::string& cs_path)
 {
-    /* 
-     * Les abbréviations suivantes sont utilisées:
-     * VS: Vertex Shader
-     * FS: Fragment Shader
-     * 
-     * 1. Lire les fichiers dans des const char*;
-     * 2. Créer les shaders, leur donner le code et les compiler;
-     * 3. Créer le programme, y attacher les shaders, et le linker;
-     * 4. Supprimer les shaders.
-    */
+    const char* cs_code = this->read_shader(cs_path);
 
-    int success;
+    GLuint cs_id = glCreateShader(GL_COMPUTE_SHADER);
+    glShaderSource(cs_id, 1, &cs_code, NULL);
+    this->compile_shader(cs_id);
+    
+    m_ID = glCreateProgram();
+    glAttachShader(m_ID, cs_id);
+    this->link_program();
 
-    // 1. Lire les fichiers dans des const char*.
-    std::string VS_code_string;
-    std::string FS_code_string;
+    glDeleteShader(cs_id);
+}
 
-    std::ifstream VS_file(VS_path);
-    if (!VS_file)
+Shader::Shader(const std::string& vs_path, const std::string& fs_path, const std::string& gs_path)
+{
+    const char *vs_code, *fs_code, *gs_code;
+    GLuint vs_id, fs_id, gs_id;
+
+    m_ID = glCreateProgram();
+
+    vs_code = this->read_shader(vs_path);
+    vs_id = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vs_id, 1, &vs_code, NULL);
+    this->compile_shader(vs_id);
+    glAttachShader(m_ID, vs_id);
+    
+    fs_code = this->read_shader(fs_path);
+    fs_id = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fs_id, 1, &fs_code, NULL);
+    this->compile_shader(fs_id);
+    glAttachShader(m_ID, fs_id);
+
+    if (!gs_path.empty())
     {
-        std::cout << "ERREUR::SHADER - Fichier vertex shader\n";
+        gs_code = this->read_shader(gs_path);
+        gs_id = glCreateShader(GL_GEOMETRY_SHADER);
+        glShaderSource(gs_id, 1, &gs_code, NULL);
+        this->compile_shader(gs_id);
+        glAttachShader(m_ID, gs_id);
     }
 
-    std::ifstream FS_file(FS_path);
-    if (!FS_file)
-    {
-        std::cout << "ERREUR::SHADER - Fichier fragment shader\n";
-    }
+    this->link_program();
 
-    std::stringstream VS;
-    std::stringstream FS;
+    glDeleteShader(vs_id);
+    glDeleteShader(fs_id);
+}
 
-    VS << VS_file.rdbuf();
-    FS << FS_file.rdbuf();
-    VS_file.close();
-    FS_file.close();
+const char* Shader::read_shader(const std::string& path)
+{
+    std::ifstream shader_file(path);
+    if (!shader_file)
+        std::cout << "ERREUR::SHADER - Fichier " << path << '\n';
 
-    VS_code_string = VS.str();
-    FS_code_string = FS.str();
+    // Extraire le texte du fichier
+    std::stringstream ss;
+    ss << shader_file.rdbuf();
+    shader_file.close();
 
-    const char* VS_code = VS_code_string.c_str();
-    const char* FS_code = FS_code_string.c_str();
+    return ss.str().c_str();
+}
 
-    // 2. Créer les shaders, leur donner le code et les compiler.
-    // Créer les deux shaders.
-    unsigned int vertex_shader = glCreateShader(GL_VERTEX_SHADER);
-    unsigned int fragment_shader = glCreateShader(GL_FRAGMENT_SHADER);
-
-    // Injecter le code source.
-    glShaderSource(vertex_shader, 1, &VS_code, NULL);
-    glShaderSource(fragment_shader, 1, &FS_code, NULL);
-
-    // Compiler et afficher les erreurs s'il y en a.
-    success = 0;
-    glCompileShader(vertex_shader);
-    glGetShaderiv(vertex_shader, GL_COMPILE_STATUS, &success);
+void Shader::compile_shader(GLuint shader_id)
+{
+    int success = 0;
+    glCompileShader(shader_id);
+    glGetShaderiv(shader_id, GL_COMPILE_STATUS, &success);
     if (success == GL_FALSE)
     {
         int length;
-        glGetShaderiv(vertex_shader, GL_INFO_LOG_LENGTH, &length);
+        glGetShaderiv(shader_id, GL_INFO_LOG_LENGTH, &length);
         char* message = new char[length];
 
-        glGetShaderInfoLog(vertex_shader, length, NULL, message);
+        glGetShaderInfoLog(shader_id, length, NULL, message);
         std::cout << "ERREUR::SHADER::COMPILATION - Vertex shader:\n";
         std::cout << message << std::endl;
 
         delete[] message;
     }
+}
 
-    success = 0;
-    glCompileShader(fragment_shader);
-    glGetShaderiv(fragment_shader, GL_COMPILE_STATUS, &success);
+void Shader::link_program()
+{
+    int success = 0;
+    glLinkProgram(m_ID);
+    glGetProgramiv(m_ID, GL_LINK_STATUS, &success);
     if (success == GL_FALSE)
     {
         int length;
-        glGetShaderiv(fragment_shader, GL_INFO_LOG_LENGTH, &length);
+        glGetProgramiv(m_ID, GL_INFO_LOG_LENGTH, &length);
         char* message = new char[length];
 
-        glGetShaderInfoLog(fragment_shader, length, NULL, message);
-        std::cout << "ERREUR::SHADER::COMPILATION - Fragment shader:\n";
-        std::cout << message << std::endl;
-
-        delete[] message;
-    }
-
-    // 3. Créer le programme, y attacher les shaders, et le linker.
-    ID = glCreateProgram();
-    glAttachShader(ID, vertex_shader);
-    glAttachShader(ID, fragment_shader);
-
-    success = 0;
-    glLinkProgram(ID);
-    glGetProgramiv(ID, GL_LINK_STATUS, &success);
-    if (success == GL_FALSE)
-    {
-        int length;
-        glGetProgramiv(ID, GL_INFO_LOG_LENGTH, &length);
-        char* message = new char[length];
-
-        glGetProgramInfoLog(ID, length, NULL, message);
+        glGetProgramInfoLog(m_ID, length, NULL, message);
         std::cout << "ERREUR::SHADER::LINK - Link programme shader:\n";
         std::cout << message << std::endl;
 
         delete[] message;
     }
-
-    // 4. Supprimer les shaders maintenant qu'on a le programme.
-    glDeleteShader(vertex_shader);
-    glDeleteShader(fragment_shader);
 }
 
 Shader::~Shader()
 {
-    glDeleteProgram(ID);
+    glDeleteProgram(m_ID);
 }
 
 void Shader::use()
 {
-    glUseProgram(ID);
+    glUseProgram(m_ID);
+}
+
+void Shader::stop()
+{
+    glUseProgram(0);
 }
 
 void Shader::set_uniform(const std::string& name, int i)
 {
-    glUniform1i(glGetUniformLocation(ID, name.c_str()), i);
+    glUniform1i(glGetUniformLocation(m_ID, name.c_str()), i);
 }
 
 void Shader::set_uniform(const std::string& name, float f)
 {
-    glUniform1f(glGetUniformLocation(ID, name.c_str()), f);
+    glUniform1f(glGetUniformLocation(m_ID, name.c_str()), f);
 }
 
 void Shader::set_uniform(const std::string& name, float f1, float f2, float f3)
 {
-    glUniform3f(glGetUniformLocation(ID, name.c_str()), f1, f2, f3);
+    glUniform3f(glGetUniformLocation(m_ID, name.c_str()), f1, f2, f3);
 }
 
 void Shader::set_uniform(const std::string& name, float f1, float f2, float f3, float f4)
 {
-    glUniform4f(glGetUniformLocation(ID, name.c_str()), f1, f2, f3, f4);
+    glUniform4f(glGetUniformLocation(m_ID, name.c_str()), f1, f2, f3, f4);
 }
 
 void Shader::set_uniform(const std::string& name, const glm::mat4& matrix)
 {
-    glUniformMatrix4fv(glGetUniformLocation(ID, name.c_str()), 1, GL_FALSE, glm::value_ptr(matrix));
+    glUniformMatrix4fv(glGetUniformLocation(m_ID, name.c_str()), 1, GL_FALSE, glm::value_ptr(matrix));
 }
--- src/Shader.h
+++ src/Shader.h
@@ -12,25 +12,30 @@
 #include "glm/glm.hpp"
 #include "glm/gtc/type_ptr.hpp"
 
-class Shader
-{
-    // Identifiant du programme shader pour OpenGL;
-    unsigned int ID;
-
+class Shader {
 public:
-    // Le constructeur lit le code source depuis deux fichiers et construit le programme shader.
-    Shader(const std::string& VS_path, const std::string& FS_path);
+    Shader() = delete;
+    Shader(const std::string& cs_path);
+    Shader(const std::string& vs_path, 
+           const std::string& fs_path,
+           const std::string& gs_path = "");
     ~Shader();
 
-    // Dire à OpenGL d'utiliser ce programme pour le rendu.
     void use();
+    void stop();
 
-    // Donner une valeur à un uniforme.
     void set_uniform(const std::string& name, int i);                                    // 1 int
     void set_uniform(const std::string& name, float f);                                  // 1 float
     void set_uniform(const std::string& name, float f1, float f2, float f3);             // 3 floats
     void set_uniform(const std::string& name, float f1, float f2, float f3, float f4);   // 4 floats
     void set_uniform(const std::string& name, const glm::mat4& matrix);                  // 1 matrice 4x4
+
+private:
+    const char* read_shader(const std::string& path);
+    void compile_shader(GLuint shader_id);
+    void link_program();
+
+    GLuint m_ID;
 };
 
 #endif
