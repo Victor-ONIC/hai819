a575ae0632e0543369da8aedf480c983a4dd8f5f
Théo Montaigu <theo.montaigu@gmail.com>
2025-03-05 16:04:53 +0100

    Modification du main pour utiliser camera + touche l pour quit

---
+++ src/Camera.cpp
@@ -0,0 +1,28 @@
+#include "Camera.h"
+
+Camera::Camera(glm::vec3 pos, glm::vec3 look_at, glm::vec3 head){
+   m_pos = pos;
+   m_look_at = look_at;
+   m_head = head;
+   m_view = glm::lookAt(m_pos, m_look_at, m_head);
+
+
+}
+
+void Camera::update(glm::vec3 pos, glm::vec3 look_at, glm::vec3 head){
+     m_pos = pos;
+     m_look_at = look_at;
+     m_head = head;
+}
+
+void Camera::pos_update(glm::vec3 pos){
+     m_pos = pos;
+  }
+
+void Camera::look_at_update(glm::vec3 look_at){
+     m_look_at = look_at;
+  }
+
+void Camera::head_update(glm::vec3 head){
+     m_head = head;
+  }
